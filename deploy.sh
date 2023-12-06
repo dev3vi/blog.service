bc60dc04ab479fbe43c054353a36cce49937f43b
thaolv <thaolv@officevnpay.com>
2023-12-06 16:59:36 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,10 @@
-#docker build -t blog .
-#docker run -p 8081:8081 blog
+#!/bin/bash
+
+git pull origin master
+mvn clean install
+docker stop blog.service
+docker rm blog.service
+docker build -t blog .
+docker run -p 8081:8081 blog.service
+
+chmod +x deploy.sh
