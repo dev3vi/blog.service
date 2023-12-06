31e0bc929afc1d81c395780e9e819efac3212d4a
thaolv <thaolv@officevnpay.com>
2023-12-06 17:44:12 +0700

    update

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@ mvn clean install
 docker stop blog.service
 docker rm blog.service
 docker build -t blog-image .
-docker run -p 8081:8081 -w /app blog-image
+docker run -p 8081:8081 blog-image
 
 chmod +x deploy.sh
