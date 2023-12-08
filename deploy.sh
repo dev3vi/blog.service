8abc35d03e23a17feea69ce2ec71c2b1d3b720a9
thaolv <thaolv@officevnpay.com>
2023-12-08 17:04:35 +0700

    update code

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,11 @@ git pull origin master
 mvn clean install
 docker stop blog.service
 docker rm blog.service
+docker rmi blog-image
 docker build -t blog-image .
-docker run -p 8081:8081 blog-image
+docker run -p 8081:8081 -d --name blog.service blog-image
+
+#Remove unused data
+#docker system prune
 
 chmod +x deploy.sh
